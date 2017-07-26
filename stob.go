e6ff66b6a7d708747c4c848e8e069b82ba42bf20
sg3des <sg3des@gmail.com>
2017-07-26 19:02:28 +0300

    totally refactoring

--- stob.go
+++ stob.go
@@ -1,15 +1,9 @@
 package stob
 
 import (
-	"encoding/binary"
-	"errors"
-	"io"
-	"log"
 	"reflect"
 	"strconv"
 	"strings"
-	"time"
-	"unsafe"
 )
 
 type Endian string
@@ -29,277 +23,340 @@ var (
 	Size64 Size = 8
 )
 
-func writeInt(w io.Writer, x interface{}, e Endian) error {
-	switch e {
-	case BigEndian:
-		return binary.Write(w, binary.BigEndian, x)
-	case LittleEndian:
-		return binary.Write(w, binary.LittleEndian, x)
-	}
-	return errors.New("unknown endian")
+type Struct struct {
+	rv reflect.Value
+	rt reflect.Type
+
+	fields []*field
 }
 
-func Write(w io.Writer, i interface{}) (err error) {
-	v := reflect.ValueOf(i)
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-	t := v.Type()
+func NewStruct(x interface{}) (*Struct, error) {
+	return newStruct(reflect.ValueOf(x).Elem())
+}
 
-	for i := 0; i < v.NumField(); i++ {
-		f := v.Field(i)
+func newStruct(rv reflect.Value) (*Struct, error) {
+	s := new(Struct)
+	s.rv = rv
+	s.rt = rv.Type()
 
-		l, e := readTag(t.Field(i))
-		if l == 0 {
-			if f.Kind() == reflect.Array || f.Kind() == reflect.Slice {
-				l = f.Len()
-			}
-		}
-
-		switch f.Interface().(type) {
-		case int:
-			switch unsafe.Sizeof(i) {
-			case 8:
-				err = writeInt(w, int64(f.Int()), e)
-			case 4:
-				err = writeInt(w, int32(f.Int()), e)
-			}
-
-		case int8:
-			err = writeInt(w, int8(f.Int()), e)
-		case int16:
-			err = writeInt(w, int16(f.Int()), e)
-		case int32:
-			err = writeInt(w, int32(f.Int()), e)
-		case int64:
-			err = writeInt(w, int64(f.Int()), e)
-
-		case byte: //same as uint8
-			w.Write([]byte{byte(f.Uint())})
-		case uint16:
-			writeInt(w, uint16(f.Uint()), e)
-		case uint32:
-			writeInt(w, uint32(f.Uint()), e)
-		case uint64:
-			writeInt(w, uint64(f.Uint()), e)
-		case uint:
-			writeInt(w, uint(f.Uint()), e)
-
-		case float32:
-			writeInt(w, float32(f.Float()), e)
-		case float64:
-			writeInt(w, f.Float(), e)
-
-		case string:
-			w.Write([]byte(f.String()))
-			w.Write(FALSE)
-
-		case []byte:
-			w.Write(getBytes(f, l))
-
-		case bool:
-			if v.Field(i).Bool() {
-				w.Write(TRUE)
-			} else {
-				w.Write(FALSE)
-			}
-
-		case time.Time:
-			b, _ := f.Interface().(time.Time).MarshalBinary()
-			w.Write(b)
-
-		default:
-
-			if l == 0 {
-				log.Printf("WARNING! for field %s сould not determine length\n", f.Type().Field(i).Name)
-				continue
-			}
-
-			if f.Kind() == reflect.Array {
-				switch f.Index(0).Interface().(type) {
-				case byte:
-					w.Write(getBytes(f, l))
-				}
-			}
-
-		}
-
+	for i := 0; i < s.rv.NumField(); i++ {
+		f, ok, err := newField(s.rv.Field(i), s.rt.Field(i))
 		if err != nil {
-			return
+			return s, err
 		}
+		if ok {
+			s.fields = append(s.fields, f)
+		}
+	}
 
+	return s, nil
+}
+
+type field struct {
+	rv  reflect.Value
+	rsf reflect.StructField
+	rk  reflect.Kind
+
+	l int
+	e Endian
+
+	read  fieldReader
+	write fieldWriter
+
+	s   *Struct
+	err error
+}
+
+func newField(rv reflect.Value, rsf reflect.StructField) (f *field, ok bool, err error) {
+	if !rv.CanSet() {
+		return nil, false, nil
+	}
+
+	f = new(field)
+	f.rv = rv
+	f.rsf = rsf
+	f.rk = rv.Kind()
+
+	f.l, f.e, ok = f.readTag(rsf.Tag.Get("stob"))
+
+	if f.l == 0 {
+		if f.rk != reflect.String {
+			f.l = int(rv.Type().Size())
+		}
+	}
+
+	if err = f.setReader(); err != nil {
+		return
+	}
+
+	if err = f.setWriter(); err != nil {
+		return
 	}
 
 	return
 }
 
-func getBytes(f reflect.Value, l int) (data []byte) {
-	data = make([]byte, l)
-
-	for i := 0; i < l; i++ {
-		data[i] = f.Index(i).Interface().(byte)
+func (f *field) readTag(tag string) (l int, e Endian, ok bool) {
+	if tag == "-" {
+		return
 	}
 
-	return
-}
-
-func readTag(sf reflect.StructField) (length int, endian Endian) {
-	ss := strings.Split(sf.Tag.Get("stob"), ",")
+	ss := strings.Split(tag, ",")
 	if len(ss) > 0 {
-		length, _ = strconv.Atoi(ss[0])
+		l, _ = strconv.Atoi(ss[0])
 	}
+
+	e = DefaultEndian
 	if len(ss) > 1 {
-		endian = Endian(ss[1])
-	} else {
-		endian = DefaultEndian
+		e = Endian(ss[1])
 	}
+
+	ok = true
 	return
 }
 
-func readInt(r io.Reader, s Size, e Endian) int64 {
-	b := make([]byte, s)
-	r.Read(b)
-	return Btoi(b, e)
-}
+// func writeInt(w io.Writer, x interface{}, e Endian) error {
+// 	switch e {
+// 	case BigEndian:
+// 		return binary.Write(w, binary.BigEndian, x)
+// 	case LittleEndian:
+// 		return binary.Write(w, binary.LittleEndian, x)
+// 	}
+// 	return errors.New("unknown endian")
+// }
 
-func Read(r io.Reader, i interface{}) {
-	v := reflect.ValueOf(i)
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-	t := v.Type()
+// func Write(w io.Writer, i interface{}) (err error) {
+// 	v := reflect.ValueOf(i)
+// 	if v.Kind() == reflect.Ptr {
+// 		v = v.Elem()
+// 	}
+// 	t := v.Type()
 
-	for i := 0; i < v.NumField(); i++ {
-		f := v.Field(i)
+// 	for i := 0; i < v.NumField(); i++ {
+// 		f := v.Field(i)
 
-		l, e := readTag(t.Field(i))
-		if l == 0 {
-			if f.Kind() == reflect.Array || f.Kind() == reflect.Slice {
-				l = f.Len()
-			}
-		}
+// 		l, e := readTag(t.Field(i))
+// 		if l == 0 {
+// 			if f.Kind() == reflect.Array || f.Kind() == reflect.Slice {
+// 				l = f.Len()
+// 			}
+// 		}
 
-		switch f.Interface().(type) {
-		case int:
-			f.SetInt(readInt(r, Size(unsafe.Sizeof(i)), e))
-		case int8:
-			f.SetInt(readInt(r, Size8, e))
-		case int16:
-			f.SetInt(readInt(r, Size16, e))
-		case int32:
-			f.SetInt(readInt(r, Size32, e))
-		case int64:
-			f.SetInt(readInt(r, Size64, e))
+// 		switch f.Interface().(type) {
+// 		case int:
+// 			switch unsafe.Sizeof(i) {
+// 			case 8:
+// 				err = writeInt(w, int64(f.Int()), e)
+// 			case 4:
+// 				err = writeInt(w, int32(f.Int()), e)
+// 			}
 
-		case uint8: //same as byte
-			f.SetUint(uint64(readInt(r, Size8, e)))
-		case uint16:
-			f.SetUint(uint64(readInt(r, Size16, e)))
-		case uint32:
-			f.SetUint(uint64(readInt(r, Size32, e)))
-		case uint64:
-			f.SetUint(uint64(readInt(r, Size64, e)))
+// 		case int8:
+// 			err = writeInt(w, int8(f.Int()), e)
+// 		case int16:
+// 			err = writeInt(w, int16(f.Int()), e)
+// 		case int32:
+// 			err = writeInt(w, int32(f.Int()), e)
+// 		case int64:
+// 			err = writeInt(w, int64(f.Int()), e)
 
-		case string:
-			f.SetString(readString(r))
+// 		case byte: //same as uint8
+// 			w.Write([]byte{byte(f.Uint())})
+// 		case uint16:
+// 			writeInt(w, uint16(f.Uint()), e)
+// 		case uint32:
+// 			writeInt(w, uint32(f.Uint()), e)
+// 		case uint64:
+// 			writeInt(w, uint64(f.Uint()), e)
+// 		case uint:
+// 			writeInt(w, uint(f.Uint()), e)
 
-		case []byte:
-			l, _ := readTag(v.Type().Field(i))
-			if l == 0 {
-				log.Printf("WARNING! for field %s сould not determine length\n", f.Type().Field(i).Name)
-				continue
-			}
-			b := make([]byte, l)
-			r.Read(b)
-			f.SetBytes(b)
+// 		case float32:
+// 			writeInt(w, float32(f.Float()), e)
+// 		case float64:
+// 			writeInt(w, f.Float(), e)
 
-		case float32:
-			var float float32
-			switch e {
-			case BigEndian:
-				binary.Read(r, binary.BigEndian, &float)
-			case LittleEndian:
-				binary.Read(r, binary.LittleEndian, &float)
-			}
-			f.SetFloat(float64(float))
+// 		case string:
+// 			w.Write([]byte(f.String()))
+// 			w.Write(FALSE)
 
-		case float64:
-			var float float64
-			switch e {
-			case BigEndian:
-				binary.Read(r, binary.BigEndian, &float)
-			case LittleEndian:
-				binary.Read(r, binary.LittleEndian, &float)
-			}
-			f.SetFloat(float)
+// 		case []byte:
+// 			w.Write(getBytes(f, l))
 
-		case bool:
-			var b = make([]byte, 1)
-			r.Read(b)
-			if b[0] == 0x01 {
-				f.SetBool(true)
-			} else {
-				f.SetBool(false)
-			}
-		case time.Time:
-			var t time.Time
-			var b = make([]byte, 15)
-			r.Read(b)
-			t.UnmarshalBinary(b)
-			f.Set(reflect.ValueOf(t))
+// 		case bool:
+// 			if v.Field(i).Bool() {
+// 				w.Write(TRUE)
+// 			} else {
+// 				w.Write(FALSE)
+// 			}
 
-		default:
+// 		case time.Time:
+// 			b, _ := f.Interface().(time.Time).MarshalBinary()
+// 			w.Write(b)
 
-			if l == 0 {
-				log.Printf("WARNING! for field %s сould not determine length", t.Field(i).Name)
-				continue
-			}
+// 		default:
 
-			if f.Kind() == reflect.Array {
-				switch f.Index(0).Interface().(type) {
-				case byte:
-					bs := make([]byte, l)
-					r.Read(bs)
+// 			if l == 0 {
+// 				log.Printf("WARNING! for field %s сould not determine length\n", f.Type().Field(i).Name)
+// 				continue
+// 			}
 
-					for i, b := range bs {
-						f.Index(i).Set(reflect.ValueOf(b))
-					}
+// 			if f.Kind() == reflect.Array {
+// 				switch f.Index(0).Interface().(type) {
+// 				case byte:
+// 					w.Write(getBytes(f, l))
+// 				}
+// 			}
 
-				}
-			}
-		}
+// 		}
 
-	}
-}
+// 		if err != nil {
+// 			return
+// 		}
 
-func Btoi(b []byte, endian Endian) (a int64) {
-	l := len(b)
-	switch endian {
-	case BigEndian:
-		for i := range b {
-			a |= int64(b[i]) << uint((l-i-1)*8)
-		}
-	case LittleEndian:
-		for i := range b {
-			a |= int64(b[i]) << uint(i*8)
-		}
-	}
+// 	}
 
-	return
-}
+// 	return
+// }
 
-func readString(r io.Reader) string {
-	var s []byte
-	for {
-		b := make([]byte, 1)
-		_, err := r.Read(b)
-		if err != nil {
-			break
-		}
-		if b[0] == 0x00 {
-			break
-		}
-		s = append(s, b[0])
-	}
-	return string(s)
-}
+// func getBytes(f reflect.Value, l int) (data []byte) {
+// 	data = make([]byte, l)
+
+// 	for i := 0; i < l; i++ {
+// 		data[i] = f.Index(i).Interface().(byte)
+// 	}
+
+// 	return
+// }
+
+// func readInt(r io.Reader, s Size, e Endian) int64 {
+// 	b := make([]byte, s)
+// 	r.Read(b)
+// 	return Btoi(b, e)
+// }
+
+// func Read(r io.Reader, i interface{}) {
+// 	v := reflect.ValueOf(i)
+// 	if v.Kind() == reflect.Ptr {
+// 		v = v.Elem()
+// 	}
+// 	t := v.Type()
+
+// 	for i := 0; i < v.NumField(); i++ {
+// 		f := v.Field(i)
+
+// 		l, e := readTag(t.Field(i))
+// 		if l == 0 {
+// 			if f.Kind() == reflect.Array || f.Kind() == reflect.Slice {
+// 				l = f.Len()
+// 			}
+// 		}
+
+// 		switch f.Interface().(type) {
+// 		case int:
+// 			f.SetInt(readInt(r, Size(unsafe.Sizeof(i)), e))
+// 		case int8:
+// 			f.SetInt(readInt(r, Size8, e))
+// 		case int16:
+// 			f.SetInt(readInt(r, Size16, e))
+// 		case int32:
+// 			f.SetInt(readInt(r, Size32, e))
+// 		case int64:
+// 			f.SetInt(readInt(r, Size64, e))
+
+// 		case uint8: //same as byte
+// 			f.SetUint(uint64(readInt(r, Size8, e)))
+// 		case uint16:
+// 			f.SetUint(uint64(readInt(r, Size16, e)))
+// 		case uint32:
+// 			f.SetUint(uint64(readInt(r, Size32, e)))
+// 		case uint64:
+// 			f.SetUint(uint64(readInt(r, Size64, e)))
+
+// 		case string:
+// 			f.SetString(readString(r))
+
+// 		case []byte:
+// 			l, _ := readTag(v.Type().Field(i))
+// 			if l == 0 {
+// 				log.Printf("WARNING! for field %s сould not determine length\n", f.Type().Field(i).Name)
+// 				continue
+// 			}
+// 			b := make([]byte, l)
+// 			r.Read(b)
+// 			f.SetBytes(b)
+
+// 		case float32:
+// 			var float float32
+// 			switch e {
+// 			case BigEndian:
+// 				binary.Read(r, binary.BigEndian, &float)
+// 			case LittleEndian:
+// 				binary.Read(r, binary.LittleEndian, &float)
+// 			}
+// 			f.SetFloat(float64(float))
+
+// 		case float64:
+// 			var float float64
+// 			switch e {
+// 			case BigEndian:
+// 				binary.Read(r, binary.BigEndian, &float)
+// 			case LittleEndian:
+// 				binary.Read(r, binary.LittleEndian, &float)
+// 			}
+// 			f.SetFloat(float)
+
+// 		case bool:
+// 			var b = make([]byte, 1)
+// 			r.Read(b)
+// 			if b[0] == 0x01 {
+// 				f.SetBool(true)
+// 			} else {
+// 				f.SetBool(false)
+// 			}
+// 		case time.Time:
+// 			var t time.Time
+// 			var b = make([]byte, 15)
+// 			r.Read(b)
+// 			t.UnmarshalBinary(b)
+// 			f.Set(reflect.ValueOf(t))
+
+// 		default:
+
+// 			if l == 0 {
+// 				log.Printf("WARNING! for field %s сould not determine length", t.Field(i).Name)
+// 				continue
+// 			}
+
+// 			if f.Kind() == reflect.Array {
+// 				switch f.Index(0).Interface().(type) {
+// 				case byte:
+// 					bs := make([]byte, l)
+// 					r.Read(bs)
+
+// 					for i, b := range bs {
+// 						f.Index(i).Set(reflect.ValueOf(b))
+// 					}
+
+// 				}
+// 			}
+// 		}
+
+// 	}
+// }
+
+// func readString(r io.Reader) string {
+// 	var s []byte
+// 	for {
+// 		b := make([]byte, 1)
+// 		_, err := r.Read(b)
+// 		if err != nil {
+// 			break
+// 		}
+// 		if b[0] == 0x00 {
+// 			break
+// 		}
+// 		s = append(s, b[0])
+// 	}
+// 	return string(s)
+// }
--- stob_test.go
+++ stob_test.go
@@ -1,121 +1,276 @@
 package stob
 
 import (
-	"bytes"
+	"encoding/hex"
+	"fmt"
 	"log"
+	"math"
 	"math/rand"
 	"testing"
 )
 
 type YourStruct struct {
-	Str    string
-	Int    int `stob:",le"`
-	Byte   byte
-	Bytes  []byte `stob:"8"`
-	Bytes4 [4]byte
-	Bool   bool
-	Float  float32 `stob:",be"`
-	Uint16 uint16
+	DstHwAddr HwAddr
+	// SrcHwAddr *HwAddr
+	Str     string
+	Int     int `stob:",le"`
+	Byte    byte
+	Bytes   []byte `stob:"6"`
+	Bytes4  [4]byte
+	Bool    bool
+	Float32 float32 `stob:",be"`
+	Uint16  uint16
+}
+
+type HwAddr struct {
+	Addr [6]byte
 }
 
 func init() {
 	log.SetFlags(log.Lshortfile)
+
+	p := []byte{1, 2, 3, 4, 5, 6}
+	arr(p[2:4])
+	fmt.Println(p)
 }
 
-func TestWriteRead(t *testing.T) {
-	buf := bytes.NewBuffer([]byte{})
+func arr(p []byte) {
+	p[0] = 10
+}
 
+// func TestBuffer(t *testing.T) {
+// 	p := make([]byte, 16)
+
+// 	buf := bytes.NewBuffer([]byte{})
+// 	buf.Write([]byte("asdasdasdasdasd01234"))
+
+// 	n, err := buf.Read(p)
+// 	log.Println(n, err)
+
+// 	fmt.Println(hex.Dump(p))
+// }
+
+func TestWriteRead(t *testing.T) {
 	a := YourStruct{
-		Str:    "string",
-		Int:    999,
-		Byte:   255,
-		Bytes:  []byte{1, 2, 3, 4, 5, 6, 7, 8},
-		Bytes4: [4]byte{10, 11, 12, 13},
-		Bool:   rand.Intn(2) == 1,
-		Float:  rand.Float32(),
-		Uint16: 65500,
+		DstHwAddr: HwAddr{[6]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
+		// SrcHwAddr: &HwAddr{[6]byte{0x55, 0x55, 0x55, 0x55, 0x55, 0x55}},
+		Str:     "string",
+		Int:     999,
+		Byte:    255,
+		Bytes:   []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		Bytes4:  [4]byte{10, 11, 12, 13},
+		Bool:    rand.Intn(2) == 1,
+		Float32: 0.98765,
+		Uint16:  65500,
 	}
 
-	if err := Write(buf, a); err != nil {
+	s, err := NewStruct(&a)
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	// fmt.Println(hex.Dump(buf.Bytes()))
+	p := make([]byte, 128)
+	n, err := s.Read(p)
+	log.Println(n, err)
+
+	fmt.Println(hex.Dump(p))
 
 	var b YourStruct
-	Read(buf, &b)
+	sb, err := NewStruct(&b)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	if b.Str != a.Str {
-		t.Error("failed read string", b.Str, a.Str)
+	n, err = sb.Write(p)
+	if err != nil {
+		t.Fatal(err)
 	}
-	if b.Int != a.Int {
-		t.Error("failed read int", b.Int, a.Int)
-	}
-	if b.Byte != a.Byte {
-		t.Error("failed read byte", b.Byte, a.Byte)
-	}
-	if !bytes.Equal(b.Bytes, a.Bytes) {
-		t.Error("failed read []byte", b.Bytes, a.Bytes)
-	}
-	if b.Bytes4 != a.Bytes4 {
-		t.Error("failed read [4]byte", b.Bytes4, a.Bytes4)
-	}
-	if b.Bool != a.Bool {
-		t.Error("failed read bool", b.Bool, a.Bool)
-	}
-	if b.Float != a.Float {
-		t.Error("failed read float32", b.Float, a.Float)
-	}
+	t.Log("writed:", n)
+	t.Logf("%+v", b)
+
+	// if err := Write(buf, a); err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// // fmt.Println(hex.Dump(buf.Bytes()))
+
+	// var b YourStruct
+	// Read(buf, &b)
+
+	// if b.Str != a.Str {
+	// 	t.Error("failed read string", b.Str, a.Str)
+	// }
+	// if b.Int != a.Int {
+	// 	t.Error("failed read int", b.Int, a.Int)
+	// }
+	// if b.Byte != a.Byte {
+	// 	t.Error("failed read byte", b.Byte, a.Byte)
+	// }
+	// if !bytes.Equal(b.Bytes, a.Bytes) {
+	// 	t.Error("failed read []byte", b.Bytes, a.Bytes)
+	// }
+	// if b.Bytes4 != a.Bytes4 {
+	// 	t.Error("failed read [4]byte", b.Bytes4, a.Bytes4)
+	// }
+	// if b.Bool != a.Bool {
+	// 	t.Error("failed read bool", b.Bool, a.Bool)
+	// }
+	// if b.Float != a.Float {
+	// 	t.Error("failed read float32", b.Float, a.Float)
+	// }
 
 	// fmt.Printf("%+v", b)
 }
 
-func BenchmarkWrite(b *testing.B) {
-	b.StopTimer()
-	a := YourStruct{
-		Str:    "string",
-		Int:    999,
-		Byte:   255,
-		Bytes:  []byte{1, 2, 3, 4, 5, 6, 7, 8},
-		Bytes4: [4]byte{10, 11, 12, 13},
-		Bool:   rand.Intn(2) == 1,
-		Float:  rand.Float32(),
-		Uint16: 65500,
+func TestItob(t *testing.T) {
+	var p = make([]byte, 32)
+	Itob(p[0:4], 255, BigEndian)
+	if p[3] != 0xff {
+		t.Error("BigEndian failed")
 	}
 
-	buf := bytes.NewBuffer([]byte{})
-	b.StartTimer()
-
-	for i := 0; i < b.N; i++ {
-		Write(buf, a)
-
-		b.StopTimer()
-		buf.Reset()
-		b.StartTimer()
+	Itob(p[4:8], 255, LittleEndian)
+	if p[4] != 0xff {
+		t.Error("LittleEndian failed")
 	}
+
+	Itob(p[8:12], int64(math.Float64bits(1.32)), BigEndian)
+
+	n := Btoi(p[8:12], BigEndian)
+	log.Println(math.Float32frombits(uint32(n)))
+
+	fmt.Println(hex.Dump(p))
 }
 
 func BenchmarkRead(b *testing.B) {
 	b.StopTimer()
 	a := YourStruct{
-		Str:    "string",
-		Int:    999,
-		Byte:   255,
-		Bytes:  []byte{1, 2, 3, 4, 5, 6, 7, 8},
-		Bytes4: [4]byte{10, 11, 12, 13},
-		Bool:   rand.Intn(2) == 1,
-		Float:  rand.Float32(),
-		Uint16: 65500,
+		Str:     "string",
+		Int:     999,
+		Byte:    255,
+		Bytes:   []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		Bytes4:  [4]byte{10, 11, 12, 13},
+		Bool:    rand.Intn(2) == 1,
+		Float32: rand.Float32(),
+		Uint16:  65500,
 	}
-	buf := bytes.NewBuffer([]byte{})
-	Write(buf, a)
+	buf := make([]byte, 128)
+
+	s, err := NewStruct(&a)
+	if err != nil {
+		b.Fatal(err)
+	}
+
 	b.StartTimer()
 
 	for i := 0; i < b.N; i++ {
-		Read(buf, &a)
-
-		// b.StopTimer()
-		// buf.Reset()
-		// b.StartTimer()
+		s.Read(buf)
 	}
 }
+
+func BenchmarkWrite(b *testing.B) {
+	b.StopTimer()
+	a := YourStruct{
+		Str:     "string",
+		Int:     999,
+		Byte:    255,
+		Bytes:   []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		Bytes4:  [4]byte{10, 11, 12, 13},
+		Bool:    rand.Intn(2) == 1,
+		Float32: rand.Float32(),
+		Uint16:  65500,
+	}
+	buf := make([]byte, 128)
+
+	s, err := NewStruct(&a)
+	if err != nil {
+		b.Fatal(err)
+	}
+	s.Read(buf)
+
+	sw, err := NewStruct(&a)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		sw.Write(buf)
+	}
+}
+
+// func BenchmarkWrite(b *testing.B) {
+// 	b.StopTimer()
+// 	a := YourStruct{
+// 		Str:    "string",
+// 		Int:    999,
+// 		Byte:   255,
+// 		Bytes:  []byte{1, 2, 3, 4, 5, 6, 7, 8},
+// 		Bytes4: [4]byte{10, 11, 12, 13},
+// 		Bool:   rand.Intn(2) == 1,
+// 		Float:  rand.Float32(),
+// 		Uint16: 65500,
+// 	}
+// 	buf := bytes.NewBuffer([]byte{})
+// 	Write(buf, a)
+// 	b.StartTimer()
+
+// 	for i := 0; i < b.N; i++ {
+// 		Read(buf, &a)
+
+// 		// b.StopTimer()
+// 		// buf.Reset()
+// 		// b.StartTimer()
+// 	}
+// }
+
+// func BenchmarkWrite(b *testing.B) {
+// 	b.StopTimer()
+// 	a := YourStruct{
+// 		Str:    "string",
+// 		Int:    999,
+// 		Byte:   255,
+// 		Bytes:  []byte{1, 2, 3, 4, 5, 6, 7, 8},
+// 		Bytes4: [4]byte{10, 11, 12, 13},
+// 		Bool:   rand.Intn(2) == 1,
+// 		Float:  rand.Float32(),
+// 		Uint16: 65500,
+// 	}
+
+// 	buf := bytes.NewBuffer([]byte{})
+// 	b.StartTimer()
+
+// 	for i := 0; i < b.N; i++ {
+// 		Write(buf, a)
+
+// 		b.StopTimer()
+// 		buf.Reset()
+// 		b.StartTimer()
+// 	}
+// }
+
+// func BenchmarkRead(b *testing.B) {
+// 	b.StopTimer()
+// 	a := YourStruct{
+// 		Str:    "string",
+// 		Int:    999,
+// 		Byte:   255,
+// 		Bytes:  []byte{1, 2, 3, 4, 5, 6, 7, 8},
+// 		Bytes4: [4]byte{10, 11, 12, 13},
+// 		Bool:   rand.Intn(2) == 1,
+// 		Float:  rand.Float32(),
+// 		Uint16: 65500,
+// 	}
+// 	buf := bytes.NewBuffer([]byte{})
+// 	Write(buf, a)
+// 	b.StartTimer()
+
+// 	for i := 0; i < b.N; i++ {
+// 		Read(buf, &a)
+
+// 		// b.StopTimer()
+// 		// buf.Reset()
+// 		// b.StartTimer()
+// 	}
+// }
